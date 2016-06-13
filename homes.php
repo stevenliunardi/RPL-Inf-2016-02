4466b3be099580fc1043bf3b7c8ab384e6c06962
putriafrah <putriafrah29@gmail.com>
2016-06-13 18:28:24 +0700

    homes.php, about.php, scripts dan slider-images

---
+++ homes.php
@@ -0,0 +1,158 @@
+﻿<?php
+	session_start();
+?>
+ 
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>FasTicket Home</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="Html5TemplatesDreamweaver.com">
+	<META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW"> <!-- Remove this Robots Meta Tag, to allow indexing of site -->
+
+    <link href="scripts/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="scripts/bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
+
+    <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <!-- Icons -->
+    <link href="scripts/icons/general/stylesheets/general_foundicons.css" media="screen" rel="stylesheet" type="text/css" />  
+    <link href="scripts/icons/social/stylesheets/social_foundicons.css" media="screen" rel="stylesheet" type="text/css" />
+    <!--[if lt IE 8]>
+        <link href="scripts/icons/general/stylesheets/general_foundicons_ie7.css" media="screen" rel="stylesheet" type="text/css" />
+        <link href="scripts/icons/social/stylesheets/social_foundicons_ie7.css" media="screen" rel="stylesheet" type="text/css" />
+    <![endif]-->
+    <link rel="stylesheet" href="scripts/fontawesome/css/font-awesome.min.css">
+    <!--[if IE 7]>
+        <link rel="stylesheet" href="scripts/fontawesome/css/font-awesome-ie7.min.css">
+    <![endif]-->
+
+    <link href="scripts/carousel/style.css" rel="stylesheet" type="text/css" />
+    <link href="scripts/camera/css/camera.css" rel="stylesheet" type="text/css" />
+
+    <link href="http://fonts.googleapis.com/css?family=Syncopate" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Abel" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Pontano+Sans" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Oxygen" rel="stylesheet" type="text/css">
+
+    <link href="styles/custom.css" rel="stylesheet" type="text/css" />
+</head>
+<body id="pageBody">
+
+<?php
+            require_once 'koneksi.php';
+        ?>
+
+<div id="divBoxed" class="container">
+
+    <div class="transparent-bg" style="position: absolute;top: 0;left: 0;width: 100%;height: 100%;z-index: -1;zoom: 1;"></div>
+
+    <div class="divPanel notop nobottom">
+            <div class="row-fluid">
+                <div class="span12">
+
+                    <div id="divLogo" class="pull-left">
+                        <a href="homes.php" id="divSiteTitle">FasTicket L300</a><br />
+                        <a href="homes.php" id="divTagLine">Fastest | Most Secure | Super Cozy</a>
+                    </div>
+
+                    <div id="divMenuRight" class="pull-right">
+                    <div class="navbar">
+                        <button type="button" class="btn btn-navbar-highlight btn-large btn-primary" data-toggle="collapse" data-target=".nav-collapse">
+                            NAVIGATION <span class="icon-chevron-down icon-white"></span>
+                        </button>
+                        <div class="nav-collapse collapse">
+                            <ul class="nav nav-pills ddmenu">
+                            <li class="active"><a href="homes.php">Home</a></li>
+                            <li><a href="about.php">About</a></li>  
+						    <?php
+              if(isset($_SESSION['email']) && $_SESSION['email'] != ""){
+ 
+          ?>
+           <li><a href="ticket.php">Ticket</a></li>
+            <?php
+          }else{
+            ?>
+            <li><a href="signup.php">Ticket</a></li>
+            
+            <?php
+          }
+          ?>  
+          <li><a href="contact.php">Contact Us</a></li>
+
+						     <?php
+              if(isset($_SESSION['email']) && $_SESSION['email'] != ""){
+
+              
+          ?>
+           <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><?php echo $_SESSION['email']; ?> <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="logout.php"><span class="fa fa-power-off fa-1x"></span>    Logout</a></li>
+              
+             
+              </li>  
+               </ul>
+            </li>
+
+            <?php
+          }else{
+            ?>
+
+            <li><a href="signup.php">Login</a></li>
+            <?php
+          }
+          ?>  
+                            </ul>
+                            </div>
+                    </div> 
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="row-fluid">
+            <div class="span12">
+
+                <div id="headerSeparator"></div>
+
+                <div class="camera_full_width">
+                    <div id="camera_wrap">
+                        <div data-src="slider-images/1.jpg" ><div class="camera_caption fadeFromBottom cap2">Fastest | Most Secure | Super Cozy</div></div>
+						<div data-src="slider-images/1.jpg" ><div class="camera_caption fadeFromBottom cap2">Fastest | Most Secure | Super Cozy</div></div>
+						
+                    </div>
+                    <br style="clear:both"/><div style="margin-bottom:40px"></div>
+                </div>               
+
+                <div id="headerSeparator2"></div>
+
+            </div>
+        </div>
+    </div>
+
+    
+            </div>
+
+           
+           
+
+<script src="scripts/jquery.min.js" type="text/javascript"></script> 
+<script src="scripts/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="scripts/default.js" type="text/javascript"></script>
+
+
+<script src="scripts/carousel/jquery.carouFredSel-6.2.0-packed.js" type="text/javascript"></script><script type="text/javascript">$('#list_photos').carouFredSel({ responsive: true, width: '100%', scroll: 2, items: {width: 320,visible: {min: 2, max: 6}} });</script><script src="scripts/camera/scripts/camera.min.js" type="text/javascript"></script>
+<script src="scripts/easing/jquery.easing.1.3.js" type="text/javascript"></script>
+<script type="text/javascript">function startCamera() {$('#camera_wrap').camera({ fx: 'scrollLeft', time: 2000, loader: 'none', playPause: false, navigation: true, height: '35%', pagination: true });}$(function(){startCamera()});</script>
+
+
+</body>
+</html>
