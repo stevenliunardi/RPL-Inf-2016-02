a27836e9c58f1c6e07f190a2b7f33039942c7952
Merita97 <itamer910@gmail.com>
2016-06-13 13:41:45 +0700

    contact.php, css, dan email.

---
+++ contact.php
@@ -0,0 +1,203 @@
+﻿<?php
+    session_start();
+?>
+
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>FasTicket Contact</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="Html5TemplatesDreamweaver.com">
+
+    <link href="scripts/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="scripts/bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
+
+    <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <!-- Icons -->
+    <link href="scripts/icons/general/stylesheets/general_foundicons.css" media="screen" rel="stylesheet" type="text/css" />  
+    <link href="scripts/icons/social/stylesheets/social_foundicons.css" media="screen" rel="stylesheet" type="text/css" />
+    <!--[if lt IE 8]>
+        <link href="scripts/icons/general/stylesheets/general_foundicons_ie7.css" media="screen" rel="stylesheet" type="text/css" />
+        <link href="scripts/icons/social/stylesheets/social_foundicons_ie7.css" media="screen" rel="stylesheet" type="text/css" />
+    <![endif]-->
+    <link rel="stylesheet" href="scripts/fontawesome/css/font-awesome.min.css">
+    <!--[if IE 7]>
+        <link rel="stylesheet" href="scripts/fontawesome/css/font-awesome-ie7.min.css">
+    <![endif]-->
+
+    <link href="scripts/wookmark/css/style.css" rel="stylesheet" type="text/css" />	<link href="scripts/yoxview/yoxview.css" rel="stylesheet" type="text/css" />
+    <link href="http://fonts.googleapis.com/css?family=Syncopate" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Abel" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Pontano+Sans" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Oxygen" rel="stylesheet" type="text/css">
+
+    <link href="styles/custom.css" rel="stylesheet" type="text/css" />
+    <script src="email/validation.js" type="text/javascript"></script>
+</head>
+<body id="pageBody">
+<?php
+            require_once 'koneksi.php';
+        ?>
+
+<div id="divBoxed" class="container">
+
+    <div class="transparent-bg" style="position: absolute;top: 0;left: 0;width: 100%;height: 100%;z-index: -1;zoom: 1;"></div>
+
+    <div class="divPanel notop nobottom">
+            <div class="row-fluid">
+                <div class="span12">
+
+                 <div id="divLogo" class="pull-left">
+                        <a href="homes.php" id="divSiteTitle">FasTicket L300</a><br />
+                        <a href="homes.php" id="divTagLine">Fastest | Most Secure | Super Cozy</a>
+                    </div>
+
+
+                    <div id="divMenuRight" class="pull-right">
+                    <div class="navbar">
+                        <button type="button" class="btn btn-navbar-highlight btn-large btn-primary" data-toggle="collapse" data-target=".nav-collapse">
+                        NAVIGATION <span class="icon-chevron-down icon-white"></span>
+                        </button>
+                            <div class="nav-collapse collapse">
+                            <ul class="nav nav-pills ddmenu">
+                            <li><a href="homes.php">Home</a></li>
+                            <li><a href="about.php">About</a></li>  
+                            <?php
+              if(isset($_SESSION['email']) && $_SESSION['email'] != ""){
+ 
+          ?>
+           <li><a href="ticket.php">Ticket</a></li>
+            <?php
+          }else{
+            ?>
+            <li><a href="signup.php">Ticket</a></li>
+            
+            <?php
+          }
+          ?>  
+          <li  class="active"><a href="contact.php">Contact Us</a></li>
+
+                             <?php
+              if(isset($_SESSION['email']) && $_SESSION['email'] != ""){
+
+              
+          ?>
+           <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><?php echo $_SESSION['email']; ?> <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="setting.php"><span class="fa fa-user fa-1x"></span>    Setting</a></li>
+                <li role="separator" class="divider"></li>
+                <li><a href="logout.php"><span class="fa fa-power-off fa-1x"></span>    Logout</a></li>
+              
+             
+              </li>
+               </ul>
+            </li>
+            
+            <?php
+          }else{
+            ?>
+
+            <li><a href="signup.php">Login</a></li>
+            <?php
+          }
+          ?>  
+                            </ul>
+                            </div>
+                    </div>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="row-fluid">
+                <div class="span12">
+                    <div id="contentInnerSeparator"></div>
+                </div>
+            </div>
+    </div>
+
+    <div class="contentArea">
+
+        <div class="divPanel notop page-content">
+
+            <div class="breadcrumbs">
+                <a href="homes.php">Home</a> &nbsp;/&nbsp; <span>Contact</span>
+            </div>
+            	
+            <div class="row-fluid">
+                <div class="span8" id="divMain">
+
+                    <h1>Contact Us</h1>
+                   	<h3 style="color:#FF6633;"><?php echo @$_GET['msg'];?></h3>
+					<hr>
+			<!--Start Contact form -->		                                                
+<form name="enq" method="post" action="email/" onsubmit="return validation();">
+  <fieldset>
+    
+	<input type="text" name="name" id="name" value=""  class="input-block-level" placeholder="Name" />
+    <input type="text" name="email" id="email" value="" class="input-block-level" placeholder="Email" />
+    <textarea rows="11" name="message" id="message" class="input-block-level" placeholder="Comments"></textarea>
+    <div class="actions">
+	<input type="submit" value="Send Your Message" name="submit" id="submitButton" class="btn btn-info pull-right" title="Click here to submit your message!" />
+	</div>
+	
+	</fieldset>
+</form>  				 
+			<!--End Contact form -->											 
+                </div>
+				
+			<!--Edit Sidebar Content here-->	
+                <div class="span4 sidebar">
+
+                    <div class="sidebox">
+                        <h3 class="sidebox-title">Contact Information</h3>
+                    <p>
+                        <address><strong>FasTicket L300</strong><br />
+                        Address<br />
+                        Jl.T.Nyak Arief, Darussalam, Banda Aceh<br />
+                        <abbr title="Phone">P:</abbr> +6281919671178</address> 
+                        <address>  <strong>Email</strong><br />
+                        <a href="mailto:#">fasticket@gmail.com</a></address>  
+                    </p>     
+                     
+					 <!-- Start Side Categories -->
+        <h4 class="sidebox-title">Our Team</h4>
+        <ul>
+          <li><a href="#">Steven Liunardi</a></li>
+          <li><a href="#">Irvaniza Patria</a></li>
+          <li><a href="#">Ridha Khalid</a></li>
+          <li><a href="#">Putri Afrah</a></li>
+          <li><a href="#">Merita</a></li>
+          <li><a href="#">Desi Mardiani</a></li>
+        </ul>
+					<!-- End Side Categories -->
+                    					
+                    </div>
+					
+					
+                    
+                </div>
+			
+
+<script src="scripts/jquery.min.js" type="text/javascript"></script> 
+<script src="scripts/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="scripts/default.js" type="text/javascript"></script>
+
+<script src="scripts/wookmark/js/jquery.wookmark.js" type="text/javascript"></script>
+<script type="text/javascript">$(window).load(function () {var options = {autoResize: true,container: $('#gridArea'),offset: 10};var handler = $('#tiles li');handler.wookmark(options);$('#tiles li').each(function () { var imgm = 0; if($(this).find('img').length>0)imgm=parseInt($(this).find('img').not('p img').css('margin-bottom')); var newHeight = $(this).find('img').height() + imgm + $(this).find('div').height() + $(this).find('h4').height() + $(this).find('p').not('blockquote p').height() + $(this).find('iframe').height() + $(this).find('blockquote').height() + 5;if($(this).find('iframe').height()) newHeight = newHeight+15;$(this).css('height', newHeight + 'px');});handler.wookmark(options);handler.wookmark(options);});</script>
+<script src="scripts/yoxview/yox.js" type="text/javascript"></script>
+<script src="scripts/yoxview/jquery.yoxview-2.21.js" type="text/javascript"></script>
+<script type="text/javascript">$(document).ready(function () {$('.yoxview').yoxview({autoHideInfo:false,renderInfoPin:false,backgroundColor:'#ffffff',backgroundOpacity:0.8,infoBackColor:'#000000',infoBackOpacity:1});$('.yoxview a img').hover(function(){$(this).animate({opacity:0.7},300)},function(){$(this).animate({opacity:1},300)});});</script>
+
+
+</body>
+</html>
